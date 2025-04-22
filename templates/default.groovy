007c36a775e6f3169345a6125b7af40dd69df9b6
HamzaAmraoui <80313251+HamzaAmraoui@users.noreply.github.com>
2025-04-22 13:00:53 +0200

    Update default.groovy

--- templates/default.groovy
+++ templates/default.groovy
@@ -1,9 +1,45 @@
+@Library('my-shared-library') _
 pipeline {
-  application_type = "java" // o "nodejs" si estás en frontend
+    agent any
 
-  stages {
-    Checkout
-    Build
-    Deploy
-  }
+    stages {
+        stage('Checkout') {
+            steps {
+                script {
+                    // Importar el stage adecuado según el tipo de aplicación
+                    if (application_type == 'java') {
+                        checkout()
+                    } else if (application_type == 'nodejs') {
+                        checkout()
+                    }
+                }
+            }
+        }
+
+        stage('Build') {
+            steps {
+                script {
+                    // Implementa la lógica de build para Java o Node.js
+                    if (application_type == 'java') {
+                        echo 'Building Java application'
+                    } else if (application_type == 'nodejs') {
+                        echo 'Building Node.js application'
+                    }
+                }
+            }
+        }
+
+        stage('Deploy') {
+            steps {
+                script {
+                    // Implementa la lógica de deploy para Java o Node.js
+                    if (application_type == 'java') {
+                        echo 'Deploying Java application'
+                    } else if (application_type == 'nodejs') {
+                        echo 'Deploying Node.js application'
+                    }
+                }
+            }
+        }
+    }
 }
